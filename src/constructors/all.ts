59dd0c779fca053bb1b93783ea023be11ca491b0
Kauã <kauagamerxp7@gmail.com>
2023-03-06 12:18:07 -0300

    new file:   src/constructors/all.ts modified:   src/constructors/database.ts

---
+++ src/constructors/all.ts
@@ -0,0 +1,4 @@
+import readDB from "../functions/readDb";
+export default function all() : object | any {
+    return readDB()
+}
--- src/constructors/database.ts
+++ src/constructors/database.ts
@@ -2,22 +2,27 @@ import fs from 'fs';
 import get from './get';
 import set  from './set';
 import remove  from './remove';
+import all from './all';
 
 interface LayerMethods {
   set: (path: string, data: any) => object,
   get: (path: string) => object | null,
   remove: (path: string) => boolean,
+  all: () => object | null
 }
 
 export class Layer implements LayerMethods {
   public set!: (path: string, data: any) => object;
   public get!: (path: string) => object | null;
   public remove!: (path: string) => boolean;
+  public all: () => object | any;
 
   constructor() {
     this.set = set;
     this.get = get;
     this.remove = remove;
+    this.all = all
+    
     const filePath = './layer.json';
     if (fs.existsSync(filePath)) {
       
